ac42c997915597bb92e8893a4eaf862c2cd98699
drincerkini <93202832+drincerkini@users.noreply.github.com>
2023-06-30 15:33:50 +0200

    working on header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
+  <HeaderComponentVue />
   <nav>
+
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/service">Service</router-link> |
@@ -7,7 +9,23 @@
     <router-link to="/register">Register</router-link>
   </nav>
   <router-view />
+
+  <FooterComponent />
+  
 </template>
+
+<script>
+import FooterComponent from '@/components/FooterComponent.vue';
+import HeaderComponentVue from './components/HeaderComponent.vue';
+
+export default {
+  components: {
+    FooterComponent,
+    HeaderComponentVue
+  }
+}
+
+</script>
 
 <style>
 #app {
