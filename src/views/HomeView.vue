f76c90feb98354a6b02063d68e3e08525677d20d
drincerkini <93202832+drincerkini@users.noreply.github.com>
2023-06-30 22:42:33 +0200

    added price section to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,9 @@
   <BannerComponent />
   <br />
   <br />
+  <OurPrices />
+  <br />
+  <br />
 <TestimonialVue />
 </template>
 
@@ -12,12 +15,14 @@
 import TestimonialVue from '@/components/TestimonialVue.vue';
 import LandingPage from '@/components/LandingPage.vue';
 import BannerComponent from '@/components/BannerComponent.vue';
+import OurPrices from '@/components/OurPrices.vue';
 
 export default {
   components: {
     TestimonialVue,
     LandingPage,
-    BannerComponent
+    BannerComponent,
+    OurPrices
   }
 }
 
