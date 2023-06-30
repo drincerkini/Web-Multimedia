55a78dc59518b08973ddf0063e6e1aada0e39259
drincerkini <93202832+drincerkini@users.noreply.github.com>
2023-06-30 23:29:14 +0200

    Added Pixi to Text

---
+++ src/components/TextPixi.vue
@@ -0,0 +1,36 @@
+<template>
+    <canvas ref="canvas"></canvas>
+  </template>
+
+  <script>
+  import * as PIXI from 'pixi.js';
+
+  export default {
+    mounted() {
+      const app = new PIXI.Application({
+        view: this.$refs.canvas,
+        width: 1200,
+        height: 150,
+        backgroundColor: 0xffffff,
+      });
+
+      const text = new PIXI.Text('LET US INTRODUCE YOU, THE BEST!', {
+        fontFamily: 'Arial',
+        fontSize: 48,
+        fill: 'brown',
+      });
+      text.anchor.set(0.5);
+      text.position.set(app.screen.width / 2, app.screen.height / 2);
+      app.stage.addChild(text);
+
+      const speed = 2;
+
+      app.ticker.add(() => {
+        text.position.x += speed;
+        if (text.position.x > app.screen.width + text.width / 2) {
+          text.position.x = -text.width / 2;
+        }
+      });
+    },
+  };
+  </script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,5 @@
 <template>
+  <TextPixiVue />
   <WhyChooseUsVue />
   <br />
   <AboutBanner />
@@ -13,13 +14,15 @@ import WhyChooseUsVue from '@/components/WhyChooseUs.vue';
 import WhoAreWeVue from '@/components/WhoAreWe.vue';
 import AboutBanner from '@/components/AboutBanner.vue';
 import AboutOurRoomSectionVue from '@/components/AboutOurRoomSection.vue';
+import TextPixiVue from '@/components/TextPixi.vue';
 
 export default {
   components: {
     WhyChooseUsVue,
     WhoAreWeVue,
     AboutBanner,
-    AboutOurRoomSectionVue
+    AboutOurRoomSectionVue,
+    TextPixiVue
   }
 }
 
